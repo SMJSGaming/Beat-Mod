db0c4c44513c35e49a24f0e447e3e76230863fb2
SMJSGaming <sjoerd.t@live.nl>
2022-05-08 19:09:16 +0200

    Made the beat more aggressive for legacy levels

--- src/hooks/PlayLayer.cpp
+++ src/hooks/PlayLayer.cpp
@@ -3,16 +3,20 @@
 
 void __fastcall PlayLayer_update_H(PlayLayer* self, void*, float unk) {
     bool isAverage = truncf(1000.0f * self->getScale()) == 1000.0f;
+    bool isNew = fromOffset<bool>(self, 0x461);
     float beatScale;
 
-    if (fromOffset<bool>(self, 0x461)) {
+    if (isNew) {
         beatScale = fromOffset<float>(FMODAudioEngine::sharedEngine(), 0x110);
     } else {
-        beatScale = fromOffset<float>(fromOffset<void*>(self, 0x380), 0x1B0) * 3.0f; // From my experience this is needed
+        beatScale = fromOffset<float>(fromOffset<void*>(self, 0x380), 0x1B0) + 0.4f; // From my experience this is needed
     }
 
     if (beatScale > 0.5f && self->getActionByTag(1001) == nullptr && pulseEnabled) {
-        CCScaleTo* scaleTo = CCScaleTo::create(0.1f, 1.0f + (beatScale - 0.5f) * 0.4f);
+        CCScaleTo* scaleTo = CCScaleTo::create(
+            0.1f, 
+            1.0f + (beatScale - 0.5f) * (0.4f + static_cast<int>(!isNew) * 0.4f)
+        );
         scaleTo->setTag(1001);
         self->stopActionByTag(1002);
         self->runAction(scaleTo);
